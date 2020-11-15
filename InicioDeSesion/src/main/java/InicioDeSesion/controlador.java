29d94319402744d4a100fbad17ef4a4a87da52fe
javikg <38766887+javikg@users.noreply.github.com>
2020-11-15 23:33:03 +0000

    Add files via upload

---
+++ InicioDeSesion/src/main/java/InicioDeSesion/controlador.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InicioDeSesion;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import javax.swing.JOptionPane;
+import model.UserDAO;
+
+
+public class controlador implements Initializable {
+
+    private UserDAO model = new UserDAO();
+    
+    @FXML
+    private TextField txtUser;
+    
+    @FXML
+    private PasswordField txtPassword;
+    
+    @FXML
+    private Button btnLogin;
+    
+    @FXML
+    private void eventKey(KeyEvent event){
+        
+        Object evt = event.getSource();
+        
+        if(evt.equals(txtUser)){
+            
+            if(event.getCharacter().equals(" ")){
+                event.consume();
+            }
+        
+        }else if(evt.equals(txtPassword)){
+
+            if(event.getCharacter().equals(" ")){
+                event.consume();
+            }            
+        
+        }
+        
+        
+        
+    }
+    
+    @FXML
+    private void eventAction(ActionEvent event){
+        
+        Object evt = event.getSource();
+
+        if(evt.equals(btnLogin)){
+                                
+            if(!txtUser.getText().isEmpty() && !txtPassword.getText().isEmpty()){
+            
+                String user = txtUser.getText();
+                String pass = txtPassword.getText();
+                
+                int state = model.login(user, pass);
+                
+                if(state!=-1){
+
+                    if(state == 1){
+
+                        JOptionPane.showMessageDialog(null, "Datos correctos puede ingresar al sistema");
+                        
+                        loadStage("/view/ViewPrincipal.fxml", event);
+
+                    }else{
+                        JOptionPane.showMessageDialog(null, "Error al iniciar sesion datos de acceso incorrectos", 
+                                                            "ADVERTENCIA", JOptionPane.WARNING_MESSAGE);                
+                    }
+
+                }                
+
+            
+            }else{            
+                    JOptionPane.showMessageDialog(null, "Error al iniciar sesion datos de acceso incorrectos", 
+                                                        "ADVERTENCIA", JOptionPane.WARNING_MESSAGE);                                
+            }
+        
+        }
+    
+    }
+    
